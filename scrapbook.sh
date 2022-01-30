be171632abdd822dede5021fdad6fddedb1dec2f
kunholee_98 <dlrjsgh750@naver.com>
2022-01-31 00:25:49 +0900

    가상환경 설정 및 실행 스크립트

---
+++ scrapbook.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# flask app, env 모두 설정해준 후 가상환경으로 진입하는 스크립트 파일입니다.
+# 현재 폴더 및 가상 환경의 위치는 개발자의 환경이므로 변경하여 사용하셔야 합니다.
+cd /Users/kunolee_98/Project/scrap-book/
+export FLASK_APP=scrap
+export FLASK_ENV=development
+
+source /Users/kunolee_98/Project/scrap-book/venv/bin/activate
---
+++ source/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return 'hello flask!'
+    return app
