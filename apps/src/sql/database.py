1fdbb9d78e6f0cd0a83c37452a754b28c44f947c
kunholee_98 <dlrjsgh750@naver.com>
2022-02-15 17:20:59 +0900

    sqlite db 이름 변경

--- apps/src/sql/database.py
+++ apps/src/sql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL="sqlite:///src/sql_app.db"
+SQLALCHEMY_DATABASE_URL="sqlite:///src/scrapbook.db"
 # SQLALCHEMY_DATABASE_URL="postgresql db endpoint"
 
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
