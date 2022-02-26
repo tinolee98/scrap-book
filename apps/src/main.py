fc9ce0fc0a446d3c5f11d0d7600c2ed9d4b55077
kunholee_98 <dlrjsgh750@naver.com>
2022-02-26 17:08:27 +0900

    cors middleware

--- apps/src/main.py
+++ apps/src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .sql.models import Base
 from .sql.database import engine, get_db
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],    
+)
 
 from .routes.apis.v1 import main_routes
 from .routes.apis.v1.auth import routes as authRoutes
