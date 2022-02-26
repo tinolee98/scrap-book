d9c058db63c1ab7f42d83083ab04a1f016763926
kunholee_98 <dlrjsgh750@naver.com>
2022-02-26 17:29:33 +0900

    cors origin regex 적용

--- apps/src/main.py
+++ apps/src/main.py
@@ -23,7 +23,7 @@ app.include_router(scrapbookRoutes.rt)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex='.*',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],    
