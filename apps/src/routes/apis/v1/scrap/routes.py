b20cf8edcf43ca65942ada300fe5f530b5743fc4
kunholee_98 <dlrjsgh750@naver.com>
2022-03-14 22:06:01 +0900

    스크랩 생성 및 이미지 업로드

--- apps/src/routes/apis/v1/scrap/routes.py
+++ apps/src/routes/apis/v1/scrap/routes.py
@@ -30,7 +30,7 @@ async def create_scrap(scrapbook_id: int, scrap: ScrapIn = Body(...), user: User
         return JSONResponse(status_code=status.HTTP_201_CREATED, content={'ok': True})
     return JSONResponse(status_code=status.HTTP_200_OK, content={'ok': False, 'error': '스크랩 생성에 실패하였습니다.'})
 
-@rt.post('/{scrapbook_id}/scrap/{scrap_id}/picture', description='스크랩 이미지 생성 API', response_model=OkError)
+@rt.put('/{scrapbook_id}/scrap/{scrap_id}/picture', description='스크랩 이미지 생성 API', response_model=OkError)
 async def create_scarap_picture(scrapbook_id: int, scrap_id: int, picture: Optional[UploadFile] = File(None), user: User = Depends(verify_token), db: Session = Depends(get_db)):
     if not user:
         return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content=error(40100))
@@ -38,11 +38,12 @@ async def create_scarap_picture(scrapbook_id: int, scrap_id: int, picture: Optio
     if not db_scrap or db_scrap.scrapbookId != scrapbook_id:
         return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=error(40400, '스크랩이 존재하지 않습니다.'))
     file_uploader = S3FileUploader(picture)
-    picture_url = file_uploader.upload()
+    picture_url = await file_uploader.upload()
     db_scrap.picture = picture_url
-    if ScrapService.update_scrap(db, db_scrap):
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content={'ok': True})
-    return JSONResponse(status_code=status.HTTP_200_OK, content={'ok': False, 'error': '이미지 업로드에 실패했습니다.'})   
+    if not ScrapService.update_scrap(db, db_scrap):
+        ScrapService.delete_scrap(db, scrap_id)
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'ok': False, 'error': '이미지 업로드에 실패했습니다.'})   
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={'ok': True})
 
 
 @rt.delete('/{scrapbook_id}/scrap/{scrap_id}', description='스크랩 삭제 API', response_model=OkError)
